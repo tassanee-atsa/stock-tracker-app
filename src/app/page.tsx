63c14b88215230cab84e306ef5217c91219cae94
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-06-21 23:00:52 +0100

    add fetch for demo data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  //www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=demo
+
+  const [stockData, setStockData] = useState({});
+
+  useEffect(() => {
+    const getStockData = async () => {
+      const stockData = await fetch(
+        "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=demo"
+      );
+      setStockData(await stockData.json());
+    };
+    getStockData();
+  }, []);
+
+  console.log(stockData);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +34,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +110,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
